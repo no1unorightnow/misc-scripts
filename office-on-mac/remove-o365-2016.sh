3bd7d9d99be7a7f62ea1d8938f41f2d5a5289389
cf8geek <cf8geek@users.noreply.github.com>
2017-07-01 12:24:57 -0500

    Create remove-o365-2016.sh

---
+++ office-on-mac/remove-o365-2016.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# removes office except dock icons
+sudo rm -rf '/Applications/Microsoft Excel.app'
+sudo rm -rf '/Applications/Microsoft Lync.app'
+sudo rm -rf '/Applications/Microsoft Messenger.app'
+sudo rm -rf '/Applications/Microsoft OneNote.app'
+sudo rm -rf '/Applications/Microsoft Outlook.app'
+sudo rm -rf '/Applications/Microsoft PowerPoint.app'
+sudo rm -rf '/Applications/Microsoft Word.app'
+sudo rm -rf '$HOME/Library/Containers/com.microsoft.errorreporting'
+sudo rm -rf '$HOME/Library/Containers/com.microsoft.Excel'
+sudo rm -rf '$HOME/Library/Containers/com.microsoft.netlib.shipassertprocess'
+sudo rm -rf '$HOME/Library/Containers/com.microsoft.Office365ServiceV2'
+sudo rm -rf '$HOME/Library/Containers/com.microsoft.Outlook'
+sudo rm -rf '$HOME/Library/Containers/com.microsoft.Powerpoint'
+sudo rm -rf '$HOME/Library/Containers/com.microsoft.RMS-XPCService'
+sudo rm -rf '$HOME/Library/Containers/com.microsoft.Word'
+sudo rm -rf '$HOME/Library/Containers/com.microsoft.onenote.mac'
+sudo rm -rf '$HOME/Library/Group Containers/UBF8T346G9.ms'
+sudo rm -rf '$HOME/Library/Group Containers/UBF8T346G9.Office'
+sudo rm -rf '$HOME/Library/Group Containers/UBF8T346G9.OfficeOsfWebHost'
+echo done.
